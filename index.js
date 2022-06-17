24963d9e865230adb59a19762316c9f78184f097
Paulo Roncarati <roncarati1209@gmail.com>
2022-06-17 14:21:37 -0300

    33 - Arquivos estaticos

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 const app = express();
 const admin = require('./routes/admin');
-const { send } = require('process');
+const path = require('path');
 // const mongoose = require("mongoose");
 
-// Configurações
+// Configurações - Início
 // Body Parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -17,6 +17,11 @@ app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // Mongoose
+
+// Configurações - Fim
+
+// Public
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Rotas
 app.get('/', (req, res) => {
