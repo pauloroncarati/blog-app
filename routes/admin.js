f9f3b265a5692531327d4e1ff8b7810d1ed1ea62
Paulo Roncarati <roncarati1209@gmail.com>
2022-06-17 22:22:00 -0300

    41 - Listando categorias

--- routes/admin.js
+++ routes/admin.js
@@ -13,7 +13,16 @@ router.get('/posts', (req, res) => {
 });
 
 router.get('/categorias', (req, res) => {
-  res.render('admin/categorias');
+  Categoria.find()
+    .sort({ date: 'desc' })
+    .lean()
+    .then((categorias) => {
+      res.render('admin/categorias', { categorias: categorias });
+    })
+    .catch((err) => {
+      req.flash('error_msg', 'Erro ao listar as categorias: ' + err);
+      res.redirect('/admin');
+    });
 });
 router.get('/categorias/add', (req, res) => {
   res.render('admin/addcategorias');
