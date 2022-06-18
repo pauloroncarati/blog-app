62318141d7760ca20b0c199edc83d103dc2b442e
Paulo Roncarati <roncarati1209@gmail.com>
2022-06-18 16:56:23 -0300

    49 - Deletando postagens

--- routes/admin.js
+++ routes/admin.js
@@ -225,4 +225,16 @@ router.post('/postagens/edit', (req, res) => {
     });
 });
 
+router.get('/postagens/deletar/:id', (req, res) => {
+  Postagem.deleteOne({ _id: req.params.id })
+    .then(() => {
+      req.flash('success_msg', 'Postagem deletada com sucesso');
+      res.redirect('/admin/postagens');
+    })
+    .catch((err) => {
+      req.flash('error_msg', 'Erro interno');
+      res.redirect('/admin/postagens');
+    });
+});
+
 module.exports = router;
