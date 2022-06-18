b9096b7a8a1e75ee084772ddd82963f066a58e89
Paulo Roncarati <roncarati1209@gmail.com>
2022-06-18 18:48:52 -0300

    50 - Criando home page

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const path = require('path');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const flash = require('connect-flash');
+require('./models/Postagem');
+const Postagem = mongoose.model('postagens');
 
 // Configurações - Início
 
@@ -53,8 +55,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Rotas
 app.get('/', (req, res) => {
-  res.send('home');
+  Postagem.find()
+    .lean()
+    .populate('categoria')
+    .sort({ data: 'desc' })
+    .then((postagens) => {
+      res.render('index', { postagens: postagens });
+    })
+    .catch((err) => {
+      req.flash('error_msg', 'Erro interno');
+      res.redirect('/404');
+    });
 });
+
+app.get('/404', (req, res) => {
+  res.send('Erro 404');
+});
+
 app.get('/posts', (req, res) => {
   res.send('posts');
 });
